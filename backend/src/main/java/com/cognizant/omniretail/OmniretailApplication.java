9f5aff92a8c39ec314dc8efb1682c47f28bef6e1
Venkatachalam <2479769@cognizant.com>
2026-03-06 17:14:38 +0530

    Committing initial skeleton

---
+++ backend/src/main/java/com/cognizant/omniretail/OmniretailApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.omniretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniretailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniretailApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cognizant/omniretail/OmniretailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.omniretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniretailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
